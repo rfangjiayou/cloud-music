571b1b40ab96e678809535181610ea976d7d2317
rfangjiayou <fangrui19941118@163.com>
2021-01-30 21:05:34 +0800

    新增推荐页

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { IconStyle } from './assets/iconfont/iconfont'
 import { GlobalStyle } from  './style'
 import { renderRoutes } from 'react-router-config'// renderRoutes 读取路由配置转化为 Route 标签
 import routes from './routes/index.js'
+import store from './store/index'
 import { HashRouter } from 'react-router-dom'
 
 const App = () => {
   return (
-    <HashRouter>
-      <GlobalStyle />
-      <IconStyle />
-      { renderRoutes(routes) }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle />
+        <IconStyle />
+        { renderRoutes(routes) }
+      </HashRouter>
+    </Provider>
   )
 }
 
